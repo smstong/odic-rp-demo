93717186da74b635be9fbbf12b7db95dff563966
JONATHAN ZHAO <jinqiangzhao2017@gmail.com>
2024-01-19 23:13:15 -0500

    Move build resources into build folder.

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+build_dir=$(cd `dirname $0`; pwd)
+project_dir=$(dirname $build_dir)
+
+cd $project_dir || exit
+
 GOOS=linux go build -o debug/oidc-client
 
 DOCKER_REGISTRY=utsso
@@ -7,7 +12,7 @@ IMAGE_VERSION=$(cat VERSION)
 IMAGE_TAG=${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMAGE_VERSION}
 IMAGE_TAG_LATEST=${DOCKER_REGISTRY}/${IMAGE_NAME}:latest
 
-docker build -t ${IMAGE_TAG} .
+docker build -t ${IMAGE_TAG} -f $build_dir/Dockerfile .
 docker tag ${IMAGE_TAG} ${IMAGE_TAG_LATEST}
 
 docker push ${IMAGE_TAG}
